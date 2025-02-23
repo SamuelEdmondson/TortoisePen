c7d72bfc5d25d7ba97e4c46475254936216dfc71
Samuel Edmondson <sedmondson@wisc.edu>
2025-02-23 13:46:51 -0600

    make the paws offset so tortoise walks like a real tortoise would

--- for_students/03-06-01.js
+++ for_students/03-06-01.js
@@ -45,11 +45,15 @@ class Tortoise {
         this.vy = 0;
         this.current_target = null;
         this.angle = 0;
-        this.speed = 5000*4;
+        this.speed = 50*4;
+        this.pawspeed = 0;
+        this.shell_wiggle = 0;
         this.components = [];
         this.shell = new Shell(30, "darkgreen");
         this.head = new Head(10, "green");
         this.frontRightPaw = new Paw(16, "green");
+        this.frontLeftPaw = new Paw(16, "green", Math.PI); // So the paws are offset, like a real tortoise!
+        this.sine_wave = 0;
         this.tail = new Tail(3, "green");
 
         this.movement_states = ["idle", "moving_toward"];
@@ -72,16 +76,17 @@ class Tortoise {
             this.frontRightPaw.draw();
             ctx?.scale(-1, 1);
             ctx?.translate(20, 0);
-            this.frontRightPaw.draw();
+            this.frontLeftPaw.draw();
             ctx?.restore();
 
             ctx?.save();
             ctx?.translate(10, 20);
-            this.frontRightPaw.draw();
+            this.frontLeftPaw.draw();
             ctx?.scale(-1, 1);
             ctx?.translate(20, 0);
             this.frontRightPaw.draw();
             ctx?.restore();
+
 
             ctx?.save();
             ctx?.translate(0, -5);
@@ -102,8 +107,12 @@ class Tortoise {
         
         console.log(this.posx);
         if (waypoints.length != 0) {
-            this.current_state = "moving_toward";
+            this.frontLeftPaw.start();
+            this.frontRightPaw.start();
 
+            this.current_state = "moving_toward";
+            this.pawspeed = 5;
+            this.shell_wiggle = 5;
             this.current_target = waypoints[0];
 
             const aiming_direction = {
@@ -113,12 +122,14 @@ class Tortoise {
 
             this.angle = Math.atan2(aiming_direction.y, aiming_direction.x) + Math.PI/2;
 
-            this.vx = this.speed * (aiming_direction.x / (aiming_direction.x**2 + aiming_direction.y**2));
-            this.vy = this.speed * (aiming_direction.y / (aiming_direction.x**2 + aiming_direction.y**2));
+            this.vx = this.speed * (aiming_direction.x / Math.sqrt(aiming_direction.x**2 + aiming_direction.y**2));
+            this.vy = this.speed * (aiming_direction.y / Math.sqrt(aiming_direction.x**2 + aiming_direction.y**2));
 
         }
         else {
             this.current_state = "idle";
+            this.frontLeftPaw.stop();
+            this.frontRightPaw.stop();
             this.vx = 0;
             this.vy = 0;
         }
@@ -126,12 +137,17 @@ class Tortoise {
         if (this.current_state == "moving_toward") {
             if (floatEquals(this.posx, this.current_target.posx, 100) && floatEquals(this.posy, this.current_target.posy, 100)) {
                 waypoints.shift();
+                this.pawspeed = 0;
+                this.shell_wiggle = 0;
                 this.current_state = "idle";
+                this.frontLeftPaw.stop();
+                this.frontRightPaw.stop();
             }
         }
 
         this.frontRightPaw.update(deltaTime);
-        this.shell.update(deltaTime);
+        this.frontLeftPaw.update(deltaTime);
+        this.shell.update(this.shell_wiggle, deltaTime);
         this.tail.update(deltaTime);
     }
 }
@@ -157,11 +173,11 @@ class Shell {
         ctx.restore();
     }
 
-    update(deltaTime) {
-        this.sinWave += deltaTime * 9;
+    update(shell_wiggle, deltaTime) {
+        this.sinWave += deltaTime * shell_wiggle;
 
-        this.posx = .5 * Math.sin(this.sinWave);
-        this.posy = .3 * Math.sin(this.sinWave);
+        this.posx = 3 * Math.sin(this.sinWave);
+        this.posy = 2 * Math.sin(this.sinWave);
     }
 }
 
@@ -185,11 +201,13 @@ class Head {
 }
 
 class Paw {
-    constructor(size, color) {
+    constructor(size, color, offset = 0) {
         this.size = size;
         this.color = color;
         this.rotation = 0;
-        this.sineWave = 0;
+        this.sineWave = 0 + offset;
+        this.pawspeed = 5;
+        this.is_stopped = false;
     }
 
     draw() {
@@ -204,8 +222,19 @@ class Paw {
     }
 
     update(deltaTime) {
-        this.sineWave += deltaTime * 5;
-        this.rotation = ((Math.PI / 8) * Math.sin(this.sineWave)) + (Math.PI / 12);
+        if (this.is_stopped) {
+            return;
+        }
+        this.sineWave += deltaTime * this.pawspeed;
+        this.rotation = ((Math.PI / 13) * Math.sin(this.sineWave)) + (Math.PI / 12);
+    }
+
+    start() {
+        this.is_stopped = false;
+    }
+
+    stop() {
+        this.is_stopped = true;
     }
 }
 
